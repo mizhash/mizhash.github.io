b1be49583fb80faf0d308b8f56b6a9945b79890e
mizhash <mizhashdev@gmail.com>
2016-05-20 22:44:07 +0300

    Update common.js

--- demos/jor/js/common.js
+++ demos/jor/js/common.js
@@ -1,4 +1,15 @@
 $(function() {
+	
+	function heightses() {
+		$(".demos-item p").height('auto').equalHeights();
+		$(".grid__item h2").height('auto').equalHeights();
+	}
+
+	$(window).resize(function() {
+		heightses();
+	});
+
+	heightses();
 
 	$(".grid__item").each(function(e) {
 
@@ -115,16 +126,7 @@ $(function() {
 		});
 	};
 
-	function heightses() {
-		$(".demos-item p").height('auto').equalHeights();
-		$(".grid__item title").height('auto').equalHeights();
-	}
-
-	$(window).resize(function() {
-		heightses();
-	});
-
-	heightses();
+	
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
